de35fbe9715b6300368016cb844ac85955dc12eb
L-Amos <luke.amos.24@ucl.ac.uk>
2025-06-25 11:00:57 +0100

    Refactoring get_tracklist into get_tracks.
    Fetches at most 20 tracks from a given playlist with a given offset.

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ class Song:
         self.artist = artist
         self.url = url
 
-def get_tracklist(playlist_id):
+def get_tracks(playlist_id, offset):
     tracklist = []
     # Get initial response to work out how many songs there are in the playlist
     response = request("GET", f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?limit=1")
-    num_tracks = response["total"]
-    for i in range(ceil(num_tracks/50)):  # Can only get 50 tracks at a time
-        response =request("GET", f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?limit=50&offset={i*50}")
-        items = response["items"]
-        for item in items:
-            name = item["track"]["name"]
-            artist = item["track"]["artists"][0]["name"]
-            url = item["track"]["uri"]
-            tracklist.append(Song(name, artist, url))
+    response =request("GET", f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?limit=20&offset={offset}")
+    items = response["items"]
+    for item in items:
+        name = item["track"]["name"]
+        artist = item["track"]["artists"][0]["name"]
+        url = item["track"]["uri"]
+        tracklist.append(Song(name, artist, url))
     return tracklist
 
 def play_song(song_url, device_id="01f5bffb-732b-4201-955a-1c0dfb727360_amzn_1"):
@@ -27,4 +25,11 @@ def play_song(song_url, device_id="01f5bffb-732b-4201-955a-1c0dfb727360_amzn_1")
     data = f'{{"uris": ["{song_url}"],"position_ms": 0}}'
     url=f"https://api.spotify.com/v1/me/player/play?device_id={device_id}"
     request("PUT", url, headers=headers, data=data)
+
+
+def main():
+    tracklist = get_tracklist("5XrU6HqhgFevHvrDE0BQT9")
+    play_song(tracklist[-1].url)
+
+main()
 
