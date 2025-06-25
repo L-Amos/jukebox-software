3054e453fa289ad9e8f82473f5be9d10da53d076
L-Amos <luke.amos.24@ucl.ac.uk>
2025-06-25 09:49:57 +0100

    Making sure get_new_token writes the new token to the correct filepath.

--- utils.py
+++ utils.py
@@ -20,13 +20,13 @@ def get_secrets(filepath):
          raise KeyError("secrets file does not have a refresh token.")
     return secrets
 
-def get_new_token(secrets):
+def get_new_token(secrets, filepath):
     headers={"Content-Type": "application/x-www-form-urlencoded"}
     data=f"grant_type=refresh_token&refresh_token={secrets["refresh_token"]}&client_id={secrets["id"]}&client_secret={secrets["client_secret"]}"
     response = requests.post(url="https://accounts.spotify.com/api/token", data=data, headers=headers, timeout=1000)
     secrets["key"] = json.loads(response.content)["access_token"]
     # Write new key to file
-    with open("secrets.json", "w") as f:
+    with open(filepath, "w") as f:
         json.dump(secrets, f)
     return secrets
 
@@ -41,7 +41,7 @@ def request(type,url, secrets_file="secrets.json", headers={}, data=None, timeou
         json_response = json.loads(response.content)
         if 'error' in json_response.keys():
             if json_response['error']['message'] == 'The access token expired':
-                secrets = get_new_token(secrets)
+                secrets = get_new_token(secrets, secrets_file)
             else:
                 raise ConnectionAbortedError(json_response)
         else:
