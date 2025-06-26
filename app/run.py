01ee9a8392494a6f2f34a0f0ebd6d45e586e0b36
L-Amos <luke.amos.24@ucl.ac.uk>
2025-06-26 19:51:41 +0100

    Adding scrolling text for too-long song titles.

--- app/run.py
+++ app/run.py
@@ -23,6 +23,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.chosen_num = ""
         self.button_list = self.buttons.findChildren(QtWidgets.QPushButton)
         self.song_labels = self.findChildren(QtWidgets.QLabel, QtCore.QRegularExpression("^song"))
+        self.timers = []
         self.page_load()
         # Connect buttons
         for button in self.button_list:
@@ -34,11 +35,46 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         song_titles = [song.title for song in self.pages[self.active_page].tracks]
         for i,title in enumerate(song_titles):
             label = self.findChild(QtWidgets.QLabel, name=f"song{i}")
-            label.setText(f"{i+1:02}: {title}")
+            label.setText(title + " ")
+            label.adjustSize()
+            scrollbar = label.parentWidget().parentWidget().parentWidget().horizontalScrollBar()
+            scrollbar.hide()
+            if len(title) > 11:
+                scrollbar.setMaximum(label.width()-label.parentWidget().parentWidget().parentWidget().width())
+                self.stop_text_scroll(scrollbar)
+        
 
     def text_reset(self):
         for label in self.song_labels:
             label.setText("")
+
+    def start_text_scroll(self, scrollbar, max):
+        scrollbar.setValue(scrollbar.value() + 20)
+        if scrollbar.value() < max:
+            timer = QtCore.QTimer()
+            timer.setInterval(500)
+            timer.setSingleShot(True)
+            timer.timeout.connect(partial(self.start_text_scroll, scrollbar, max))
+            timer.start()
+            self.timers.append(timer)
+        else:
+            timer = QtCore.QTimer()
+            timer.setInterval(3000)
+            timer.setSingleShot(True)
+            timer.timeout.connect(partial(self.stop_text_scroll, scrollbar))
+            timer.start()
+            self.timers.append(timer)
+        
+
+    def stop_text_scroll(self, scrollbar):
+        scrollbar.setValue(0)
+        timer = QtCore.QTimer()
+        timer.setInterval(3000)
+        timer.setSingleShot(True)
+        timer.timeout.connect(partial(self.start_text_scroll, scrollbar, scrollbar.maximum()))
+        self.timers.append(timer)
+        timer.start()
+        
 
     def button_reset(self):
         for button in self.button_list:
@@ -69,6 +105,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     self.playing = True
                     self.button_reset()
         else:
+            self.clear_timers()
             if "forward" in button.objectName():
                 self.active_page += 1
                 if self.active_page >= len(self.pages):
@@ -80,6 +117,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.pages[self.active_page].refresh()
             self.page_load()
             self.button_reset()
+
+    def clear_timers(self):
+        for timer in self.timers:
+            timer.stop()
+        self.timers = []
 
                 
 
